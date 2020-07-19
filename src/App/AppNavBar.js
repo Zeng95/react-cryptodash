de8fb20fb2164cdefb902982f6c6461fc610d819
Zeng Tao <qingchun1995@gmail.com>
2020-07-19 16:51:34 +0800

    Style AppNavBar

--- src/App/AppNavBar.js
+++ src/App/AppNavBar.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const AppNavBarStyled = styled.header`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `
 
+const Logo = styled.h1`
+  font-size: 1.5em;
+  margin: 0;
+`
+
+const ControlButtonElem = styled.button`
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  color: white;
+  cursor: pointer;
+
+  ${props =>
+    props.active &&
+    css`
+      color: red;
+    `}
+`
+
+class ControlButton extends Component {
+  capitalize(str) {
+    // 首字母大写，其余字母小写
+    return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase()
+  }
+
+  render() {
+    const { name, active } = this.props
+
+    return (
+      <ControlButtonElem active={active}>
+        {this.capitalize(name)}
+      </ControlButtonElem>
+    )
+  }
+}
+
 class AppNavBar extends Component {
   render() {
     return (
       <AppNavBarStyled>
-        <div>CryptoDash</div>
+        <Logo>CryptoDash</Logo>
         <div className="text-transparent">Space Remaining</div>
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <ControlButton name="dashboard" active />
+        <ControlButton name="settings" />
       </AppNavBarStyled>
     )
   }
