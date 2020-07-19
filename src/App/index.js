10c5bade9e276427a8decf103e8c5a1a793d77bb
Zeng Tao <qingchun1995@gmail.com>
2020-07-19 11:47:25 +0800

    Add Google Fonts

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled, { css } from 'styled-components'
 import './App.css'
 
+const MyButton = styled.div`
+  color: green;
+  ${props =>
+    props.primary &&
+    css`
+      border: 1px solid yellow;
+    `}
+`
+
+class Welcome extends Component {
+  render() {
+    return <h1>Welcome to CryptoDash</h1>
+  }
+}
+
 function App() {
-  return <div className="App">Hello World</div>
+  return (
+    <div className="App">
+      <Welcome />
+      <MyButton primary>My Button</MyButton>
+    </div>
+  )
 }
 
 export default App
