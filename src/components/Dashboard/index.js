1bc0412016504ec2b171fdf0d2a13f09911ccd8a
Zeng Tao <qingchun1995@gmail.com>
2020-08-10 15:18:31 +0800

    Add Dashboard folder and PriceGird.js

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Page from '../Shared/Page'
+
+class Dashboard extends Component {
+  render() {
+    return <Page name="dashboard">I'm dashboard</Page>
+  }
+}
+
+export default Dashboard
