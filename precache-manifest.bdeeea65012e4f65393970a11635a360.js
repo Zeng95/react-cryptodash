8280db2fe5408856a938344c62998a4d5f34deb5
Zeng Tao <qingchun1995@gmail.com>
2020-08-25 16:11:08 +0800

    Updates

---
+++ precache-manifest.bdeeea65012e4f65393970a11635a360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7562a2456a6fe77645a4affa46821b0c",
+    "url": "/react-cryptodash/index.html"
+  },
+  {
+    "revision": "b5f33843a06c9dfdcd93",
+    "url": "/react-cryptodash/static/css/main.bb3e42dc.chunk.css"
+  },
+  {
+    "revision": "ed2da2fdbdd94de63d30",
+    "url": "/react-cryptodash/static/js/2.e90f5a6e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-cryptodash/static/js/2.e90f5a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f33843a06c9dfdcd93",
+    "url": "/react-cryptodash/static/js/main.3c6a2679.chunk.js"
+  },
+  {
+    "revision": "3a6e6ad4a9e20167c140",
+    "url": "/react-cryptodash/static/js/runtime-main.4db23ac9.js"
+  }
+]);
